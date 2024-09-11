2c67af20c9d3fa2d4262f13dcb73f5757fe1a319
Sudhanshu kumar <sudhanshu196r@gmail.com>
2024-09-11 11:05:52 +0530

    ADDED functionality switch case

--- src/com/bridgelabz/employee_wage/EmployeeWage.java
+++ src/com/bridgelabz/employee_wage/EmployeeWage.java
@@ -38,42 +38,77 @@ public class EmployeeWage {
 			System.out.println("The daily wage of Employee is : 0 " );
 			System.out.println("");
 		}
-		
-		
-		
+
+
+
 
 		/*! UC3*/
-		
-		 int wagePerHour1 = 20;
-       int fullDay1 = 8;
-       int halfDay = 4;
-       System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
-       System.out.println("");
 
-       int empCheck2 = (int) Math.floor(Math.random() * 10) % 3;
-       if( empCheck2 == 0 )
-       {
-           System.out.print("Employee is Present");
-           System.out.println("");
-           System.out.print("The daily wage of Employee is : " + ( wagePerHour1 * fullDay1 ));
-           System.out.println("");
-       }
+		int wagePerHour1 = 20;
+		int fullDay1 = 8;
+		int halfDay = 4;
+		System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
+		System.out.println("");
 
-       else if ( empCheck2 == 1 )
+		int empCheck2 = (int) Math.floor(Math.random() * 10) % 3;
+		if( empCheck2 == 0 )
+		{
+			System.out.print("Employee is Present");
+			System.out.println("");
+			System.out.print("The daily wage of Employee is : " + ( wagePerHour1 * fullDay1 ));
+			System.out.println("");
+		}
 
-       {
-           System.out.print("Employee is Present but working part time ");
-           System.out.println("");
-           System.out.print("The daily wage of Employee is : " + ( wagePerHour1 * halfDay ));
-           System.out.println("");
-       }
-       else
-       {
-           System.out.print("Employee is Absent");
-           System.out.println("");
-           System.out.println("The daily wage of Employee is : 0 " );
-           System.out.println("");
-       }
+		else if ( empCheck2 == 1 )
+
+		{
+			System.out.print("Employee is Present but working part time ");
+			System.out.println("");
+			System.out.print("The daily wage of Employee is : " + ( wagePerHour1 * halfDay ));
+			System.out.println("");
+		}
+		else
+		{
+			System.out.print("Employee is Absent");
+			System.out.println("");
+			System.out.println("The daily wage of Employee is : 0 " );
+			System.out.println("");
+		}
+
+
+
+		/*UC4*/
+
+		int wagePerHour4 = 20;
+		int fullDay4 = 8;
+		int halfDay4 = 4;
+		System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
+		System.out.println("");
+
+		int empCheck4 = (int) Math.floor(Math.random() * 10) % 3;
+		switch ( empCheck4 ) {
+		case 0:
+			System.out.print("Employee is Present");
+			System.out.println("");
+			System.out.print("The daily wage of Employee is : " + (wagePerHour4 * fullDay4));
+			System.out.println("");
+			break;
+
+		case 1:
+			System.out.print("Employee is Present but working part time ");
+			System.out.println("");
+			System.out.print("The daily wage of Employee is : " + (wagePerHour4 * halfDay4));
+			System.out.println("");
+			break;
+
+		case 2:
+			System.out.print("Employee is Absent");
+			System.out.println("");
+			System.out.println("The daily wage of Employee is : 0 ");
+			System.out.println("");
+			break;
+
+		}
 
 	}
 }
