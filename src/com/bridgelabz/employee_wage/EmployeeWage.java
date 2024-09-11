ed113182232287a85af669dfd626fd888c6c88f8
Sudhanshu kumar <sudhanshu196r@gmail.com>
2024-09-11 10:46:52 +0530

    MODIFIED main

--- src/com/bridgelabz/employee_wage/EmployeeWage.java
+++ src/com/bridgelabz/employee_wage/EmployeeWage.java
@@ -18,5 +18,28 @@ public class EmployeeWage {
 
 
 
+		int wagePerHour = 20;
+		int fullDay = 8;
+		System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
+		System.out.println("");
+
+		int empCheck1 = (int) Math.floor(Math.random() * 10) % 2;
+		if(empCheck1 == 0)
+		{
+			System.out.print("Employee is Present");
+			System.out.println("");
+			System.out.print("The daily wage of Employee is : " + ( wagePerHour * fullDay ));
+			System.out.println("");
+		}
+		else
+		{
+			System.out.print("Employee is Absent");
+			System.out.println("");
+			System.out.println("The daily wage of Employee is : 0 " );
+			System.out.println("");
+		}
+
+
+
 	}
-	}
+}
