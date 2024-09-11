a47ac7d432b37fc2cfcdbf2579b4075fae319f50
Sudhanshu kumar <sudhanshu196r@gmail.com>
2024-09-11 11:24:47 +0530

    ADDED functionality wage condition

--- src/com/bridgelabz/employee_wage/EmployeeWage.java
+++ src/com/bridgelabz/employee_wage/EmployeeWage.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.employee_wage;
 
+import java.util.Scanner;
+
 public class EmployeeWage {
 
 	public static void main(String[] args) {
@@ -143,6 +145,62 @@ public class EmployeeWage {
 			System.out.println("");
 			break;
 
+		}
+
+		/*UC6*/
+
+
+		int wagePerHour6 = 20;
+		int fullDay6 = 8;
+		int halfDay6 = 4;
+		int totalNoOfDays6=0;
+		int dailyWage = 0;
+		int monthlyWage = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
+		System.out.println("");
+		System.out.print("Enter the number of days you were supposed to work ( between 1 -20 ): ");
+		totalNoOfDays = sc.nextInt();
+		int totalHours = totalNoOfDays6 * fullDay6;
+		int counter = 0;
+		for (int i = 1; i <= totalNoOfDays6; i++) {
+
+			int empCheck6 = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck6) {
+			case 0:
+
+				System.out.println("Employee is Present on day and is working full time : " + i);
+				dailyWage = wagePerHour6 * fullDay6;
+				System.out.println("The daily wage of Employee is : " + dailyWage);
+				System.out.println(" \n ");
+				counter = counter + 1;
+				break;
+
+			case 1:
+				System.out.print("Employee is Present but working part time on day : " + i);
+				System.out.println("");
+				dailyWage = wagePerHour6 * halfDay6;
+				System.out.println("The daily wage of Employee is : " + dailyWage);
+				System.out.println(" \n ");
+				break;
+
+			case 2:
+				System.out.println("Employee is Absent on day :" + i);
+				dailyWage = 0;
+				System.out.println("The daily wage of Employee is : " + dailyWage);
+				System.out.println(" \n ");
+				break;
+			}
+
+			monthlyWage = monthlyWage + dailyWage;
+		}
+		System.out.println("\n");
+		if(totalHours >= 100 || counter >= 20) {
+			System.out.println("The monthly wage of employee is : " + monthlyWage);
+		}
+		else
+		{
+			System.out.println("The monthly wage of employee is : " + monthlyWage);
 		}
 
 	}
