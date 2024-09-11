faadda90f07d2907da59c5d2e116a4d5fa69155a
Sudhanshu kumar <sudhanshu196r@gmail.com>
2024-09-11 10:44:39 +0530

    MODIFIED main

--- src/com/bridgelabz/employee_wage/EmployeeWage.java
+++ src/com/bridgelabz/employee_wage/EmployeeWage.java
@@ -2,18 +2,21 @@ package com.bridgelabz.employee_wage;
 
 public class EmployeeWage {
 
-
-	        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-	        if(empCheck == 0)
-	        {
-	            System.out.println("Employee is Present");
-	        }
-	        else
-	        	
-	        {
-	            System.out.println("Employee is Absent");
-	        }
+	public static void main(String[] args) {
 
 
-	
-}
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		if(empCheck == 0)
+		{
+			System.out.println("Employee is Present");
+		}
+		else
+
+		{
+			System.out.println("Employee is Absent");
+		}
+
+
+
+	}
+	}
