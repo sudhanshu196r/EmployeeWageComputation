dd79cae1cf378f7b4b67b805ca0ba034944bfbdd
Sudhanshu kumar <sudhanshu196r@gmail.com>
2024-09-11 09:47:44 +0530

    ADDED functunality daily employee wage

--- src/com/bridgelabz/employee_wage/EmployeeWage.java
+++ src/com/bridgelabz/employee_wage/EmployeeWage.java
@@ -5,8 +5,8 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		 System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
 
-	        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-	        if(empCheck == 0)
+	        int empCheck1 = (int) Math.floor(Math.random() * 10) % 2;
+	        if(empCheck1 == 0)
 	        {
 	            System.out.println("Employee is Present");
 	        }
@@ -14,6 +14,28 @@ public class EmployeeWage {
 	        {
 	            System.out.println("Employee is Absent");
 	        }
+	        
+	        int wagePerHour = 20;
+	        int fullDay = 8;
+	        System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
+	        System.out.println("");
+
+	        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+	        if(empCheck == 0)
+	        {
+	            System.out.print("Employee is Present");
+	            System.out.println("");
+	            System.out.print("The daily wage of Employee is : " + ( wagePerHour * fullDay ));
+	            System.out.println("");
+	        }
+	        else
+	        {
+	            System.out.print("Employee is Absent");
+	            System.out.println("");
+	            System.out.println("The daily wage of Employee is : 0 " );
+	            System.out.println("");
+	        }
+
 	}
 
 }
