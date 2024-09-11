8388e62cf601e602d8afffa82505aa6a68390667
Sudhanshu kumar <sudhanshu196r@gmail.com>
2024-09-11 09:31:49 +0530

    ADDED employee attendance feature

--- src/com/bridgelabz/employee_wage/EmployeeWage.java
+++ src/com/bridgelabz/employee_wage/EmployeeWage.java
@@ -3,7 +3,17 @@ package com.bridgelabz.employee_wage;
 public class EmployeeWage {
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		 System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
+
+	        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+	        if(empCheck == 0)
+	        {
+	            System.out.println("Employee is Present");
+	        }
+	        else
+	        {
+	            System.out.println("Employee is Absent");
+	        }
 	}
 
 }
